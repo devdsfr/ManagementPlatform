670f49644c5184f0e51e873b788ddc0f31d0fc96
Daniel Ramos <44585550+devdsfr@users.noreply.github.com>
2024-08-25 12:04:30 -0300

    add frontend

---
+++ gestorpedidos/src/test/java/com/plataforma/gestorpedidos/GestorpedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plataforma.gestorpedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorpedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
